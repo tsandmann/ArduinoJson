ace0ff7d7df5a577a7bc8d07e004d0de48d71b92
Timo Sandmann <mail@timosandmann.de>
2019-07-14 21:36:37 +0200

    disable warnings on clang for emtpy modules

---
+++ src/dummy.cpp
@@ -0,0 +1,14 @@
+// ArduinoJson - arduinojson.org
+// Copyright Benoit Blanchon 2014-2019
+// MIT License
+
+/**
+ * @file    dummy.cpp
+ * @brief   Empty dummy file to make clang's ar happy
+ * @author  Timo Sandmann
+ * @date    14.07.2019
+ */
+
+#ifdef __clang__
+int dummy;
+#endif
